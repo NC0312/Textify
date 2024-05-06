7a00e4ab2469ecb10d2299bf291e3fbc2c0edb04
NC0312 <niketchawla983@gmail.com>
2024-05-07 01:58:16 +0530

    Final Commit-socket

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,40 +1,58 @@
-import React, { useEffect ,useRef } from 'react'
-import Message from './Message.jsx';
-import useGetMessages from '../../hooks/useGetMessages.js';
-import MessageSkeleton from '../skeletons/MessageSkeleton.jsx';
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
-  const { messages, loading } = useGetMessages();
+	const { messages, loading } = useGetMessages();
+	useListenMessages();
+	const lastMessageRef = useRef();
 
-  const lastMessageRef=useRef();
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      lastMessageRef.current ?.scrollIntoView({behavior:"smooth"});
-    },100)
-  },[messages]);
+	return (
+		<div className='px-4 flex-1 overflow-auto'>
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
+				))}
 
-  return (
-    <div className='px-4 flex-1 overflow-auto'>
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
+		</div>
+	);
+};
+export default Messages;
 
-      {!loading &&
-        messages.length > 0 &&
-        messages.map((message)=>
-           <div key={message._id} ref={lastMessageRef} >
-            <Message message={message}/>
-           </div>
-        )}
+// STARTER CODE SNIPPET
+// import Message from "./Message";
 
-      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-
-      {
-        !loading && messages.length === 0 && (
-          <p className='text-center'>Send a message to start the conversation</p>
-        )
-      }
-
-    </div>
-  )
-}
-
-export default Messages
+// const Messages = () => {
+// 	return (
+// 		<div className='px-4 flex-1 overflow-auto'>
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 		</div>
+// 	);
+// };
+// export default Messages;
